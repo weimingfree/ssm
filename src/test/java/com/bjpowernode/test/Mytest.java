4323e7f1039cf25b0ead3881b378b658e2a13d13
weimingfree <3381261694@qq.com>
2022-10-11 19:52:26 +0800

    Commit Message

---
+++ src/test/java/com/bjpowernode/test/Mytest.java
@@ -0,0 +1,35 @@
+package com.bjpowernode.test;
+
+import com.bjpowernode.mapper.ProductInfoMapper;
+import com.bjpowernode.pojo.ProductInfo;
+import com.bjpowernode.pojo.vo.ProductInfoVo;
+import com.bjpowernode.utils.MD5Util;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext_dao.xml","classpath:applicationContext_service.xml"})
+public class Mytest {
+   /* @Test
+    public void testMd5(){
+        String mi = MD5Util.getMD5("000000");
+        System.out.println(mi);*/
+    @Autowired
+    ProductInfoMapper mapper;
+
+    @Test
+    public void testSelectCondition(){
+        ProductInfoVo vo = new ProductInfoVo();
+        vo.setPname("4");
+        vo.setTypeid(3);
+        vo.setLprice(3000);
+        vo.setHprice(3999);
+        List<ProductInfo> list = mapper.selectCondition(vo);
+        list.forEach(productInfo->System.out.println(productInfo));
+    }
+}
